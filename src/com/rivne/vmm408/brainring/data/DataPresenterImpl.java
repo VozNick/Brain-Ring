3835231275ed4dc33e638f096495d4d9066529a3
Voz4nick <vmm408@gmail.com>
2020-02-01 22:56:48 +0200

    edit DataPresenterImpl

--- src/com/rivne/vmm408/brainring/data/DataPresenterImpl.java
+++ src/com/rivne/vmm408/brainring/data/DataPresenterImpl.java
@@ -9,29 +9,49 @@ import java.util.List;
 import java.util.Random;
 
 public class DataPresenterImpl {
-    private static List<Integer> integerList = new LinkedList<>();
-    private static List<TeamModel> teamModelList = new ArrayList<>();
-    private static List<DuelModel> duelModelList = new ArrayList<>();
+    private List<Integer> integerList = new LinkedList<>();
+    private List<TeamModel> teamModelList = new ArrayList<>();
+    private List<DuelModel> duelModelList = new ArrayList<>();
+    private int duelNum;
+    private int teamNum;
+    private int questionNumPerTDuel;
+    private int questionNumPerTeam;
+    private int questionNum;
 
-    public static int duelNum;
-    public static int teamNum;
-    public static int questionNumPerTDuel;
-    public static int questionNumPerTeam;
-    public static int questionNum;
-
-    public static List<Integer> getIntegerList() {
+    public List<Integer> getIntegerList() {
         return integerList;
     }
 
-    public static List<TeamModel> getTeamModelList() {
+    public List<TeamModel> getTeamModelList() {
         return teamModelList;
     }
 
-    public static List<DuelModel> getDuelModelList() {
+    public List<DuelModel> getDuelModelList() {
         return duelModelList;
     }
 
-    public static void removeAllData() {
+
+    public void setTeamNum(int teamNum) {
+        this.teamNum = teamNum;
+    }
+
+    public void setQuestionNumPerTeam(int questionNumPerTeam) {
+        this.questionNumPerTeam = questionNumPerTeam;
+    }
+
+    public void setDuelNum(int duelNum) {
+        this.duelNum = duelNum;
+    }
+
+    public void setQuestionNumPerTDuel(int questionNumPerTDuel) {
+        this.questionNumPerTDuel = questionNumPerTDuel;
+    }
+
+    public void setQuestionNum(int questionNum) {
+        this.questionNum = questionNum;
+    }
+
+    public void removeAllData() {
         integerList.clear();
         teamModelList.clear();
         duelModelList.clear();
@@ -42,43 +62,41 @@ public class DataPresenterImpl {
         questionNum = 0;
     }
 
-    public static void generateOriginalListOfQuestions() {
+    public void generateOriginalListOfQuestions() {
         for (int i = 0; i < questionNum; i++) {
             integerList.add(i + 1);
         }
     }
 
-    public static void setRandomQuestionsForTeams() {
+    public void setRandomQuestionsForTeams() {
         for (int i = 0; i < teamNum; i++) {
             TeamModel teamModel = new TeamModel();
             teamModel.setId(i + 1);
-            List<Integer> integers = new ArrayList<>();
-            for (int j = 0; j < questionNumPerTeam; j++) {
-                int random = getRandomNum();
-                integers.add(integerList.get(random));
-                integerList.remove(random);
-            }
-            teamModel.setIntegerList(integers);
+            teamModel.setIntegerList(returnIntegerList(questionNumPerTeam));
             teamModelList.add(teamModel);
         }
     }
 
-    public static void setRandomQuestionsForDuels() {
+    public void setRandomQuestionsForDuels() {
         for (int i = 0; i < duelNum; i++) {
             DuelModel duelModel = new DuelModel();
             duelModel.setId(i + 1);
-            List<Integer> integers = new ArrayList<>();
-            for (int j = 0; j < questionNumPerTDuel; j++) {
-                int random = getRandomNum();
-                integers.add(integerList.get(random));
-                integerList.remove(random);
-            }
-            duelModel.setIntegerList(integers);
+            duelModel.setIntegerList(returnIntegerList(questionNumPerTDuel));
             duelModelList.add(duelModel);
         }
     }
 
-    private static int getRandomNum() {
+    private List<Integer> returnIntegerList(int count) {
+        List<Integer> integers = new ArrayList<>();
+        for (int j = 0; j < count; j++) {
+            int random = getRandomNum();
+            integers.add(integerList.get(random));
+            integerList.remove(random);
+        }
+        return integers;
+    }
+
+    private int getRandomNum() {
         return new Random().nextInt(integerList.size());
     }
 }
